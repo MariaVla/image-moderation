e405c3e2e2ee9e5cfc46858fdb15abb1bec0a662
Majo Vlasanovich <majo@vlasanovich.com>
2021-01-22 06:38:43 -0300

    Check if user response from the backend has an id before signin.

--- src/components/Register.js
+++ src/components/Register.js
@@ -22,7 +22,7 @@ const Register = ({ loadUser, onRouteChange }) => {
     })
       .then((response) => response.json())
       .then((user) => {
-        if (user) {
+        if (user.id) {
           loadUser(user);
           onRouteChange('home');
         }
